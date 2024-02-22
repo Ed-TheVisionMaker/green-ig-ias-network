6aa9060b2710f5e8cabe6baf7f62756dc1083234
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-22 23:04:29 +0000

    styled navbar buttons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,13 +5,13 @@ const Navbar: FC = () => {
   return (
     <div>
       <div className='bg-black flex justify-end py-4 pr-8 space-x-4'>
-        <Link className='text-[#7CEA23]' href='/welcome'>
+        <Link className='text-[#7CEA23] text-lg border-2 border-transparent hover:border-2 hover:border-white py-1 px-2 rounded-xl' href='/welcome'>
           welcome
         </Link>
-        <Link className='text-[#7CEA23]' href='/invasive'>
+        <Link className='text-[#7CEA23] text-lg border-2 border-transparent hover:border-2 hover:border-white py-1 px-2 rounded-xl' href='/invasive'>
           the invasive species
         </Link>
-        <Link className='text-[#7CEA23]' href='/community'>
+        <Link className='text-[#7CEA23] text-lg border-2 border-transparent hover:border-2 hover:border-white py-1 px-2 rounded-xl' href='/community'>
           community
         </Link>
       </div>
