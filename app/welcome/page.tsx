19f0718790925f848f93eca439ccad1c23d923fe
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-22 23:15:04 +0000

    some styling applied to the invasive page

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -2,11 +2,11 @@ import React, { FC } from 'react';
 
 const Welcome: FC = () => {
   return (
-    <div className='flex flex-col m-48 text-center h-screen'>
-      <h1 className='font-bold text-3xl'>
+    <div className='flex flex-col m-36 text-center h-screen'>
+      <h1 className='font-bold text-4xl'>
         Welcome to the IAS Green Iguana Network
       </h1>
-      <p className='text-xl m-12'>
+      <p className='text-2xl m-12'>
         A place for people to connect and collaborate to control <br />
         the spread of green iguanas as an Invasive Alien Species
       </p>
