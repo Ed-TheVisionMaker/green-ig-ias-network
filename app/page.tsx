cd7657e0da266b73e81bfb6d89baba23cccd3e60
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-24 21:33:26 +0000

    setup rewrites as proxy in next config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+'use client';
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import React, { FC, useEffect, useState } from 'react';
 
-export default Home
+const Home: FC = () => {
+  const [users, setUsers] = useState<string[] | null>(null);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      // don't need localhost - react doesn't recognise it as a valid origin
+      // proxy in front of the server handles this in package.json
+      // for production need cors and to change endpoints
+      const response = await fetch('/api/users');
+      const json = await response.json();
+      console.log(json)
+      if (response.ok) {
+        setUsers(json);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+  return <div>home</div>;
+};
+
+export default Home;
---
+++ components/UsersButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsersButton = () => {
+  return (
+    <div>
+        <button>Get All Users</button>
+    </div>
+  )
+}
+
+export default UsersButton
