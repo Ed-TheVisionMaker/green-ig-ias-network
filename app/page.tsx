1ef5933000256027c4377e643abc2195956b4a12
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-26 22:25:48 +0000

    resolved error with obsolete api call causing a 404

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
 'use client';
 
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import UserForm from '@/components/UserForm';
 
 const Home: FC = () => {
-  const [users, setUsers] = useState<string[] | null>(null);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      // for production need cors and to change endpoints
-      // currently local host handled in next.config.mjs rewrite function
-      const response = await fetch('/api/users');
-      const json = await response.json();
-      console.log(json);
-      if (response.ok) {
-        setUsers(json);
-      }
-    };
-
-    fetchUsers();
-  }, []);
   return (
     <div>
       <UserForm />
