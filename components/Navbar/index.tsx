a31b2796baaa28c44e456d5f27d8e9b52cdcb25b
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-03-26 17:48:26 +0000

    style: logo changed and sized correctly

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
               <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
                 <div className='flex flex-shrink-0 items-center'>
                   <img
-                    className='h-24'
+                    className='h-14'
                     src='/ias-gin-logo-white.png'
                     alt='Your Company'
                   />
