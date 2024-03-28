a7cc1483cf3a44f6b15c2dffef5c797d4a9a04df
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-03-28 22:11:28 +0000

    style: improved waitlist styling

--- components/Waitlist.tsx/index.tsx
+++ components/Waitlist.tsx/index.tsx
@@ -32,15 +32,17 @@ export default function Waitlist() {
     return (
         <div 
         id='waitlist'
-        className="relative rounded-3xl bg-teaGreen p-6">
+        className="relative mt-16 ">
             {showAnimation && (
                 <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-l from-gray-900 to-transparent"></div>
             )}
             <div className="">
-                <h2 className="font-bold text-2xl tracking-tight text-ginBlack">
+                <div>
+                <h2 className="font-bold text-3xl tracking-tight text-ginBlack">
                     Get notified when we're launching.
                 </h2>
-                <form className="w-full max-w-md mt-4" onSubmit={handleSubmit}>
+                </div>
+                <form className="mt-8 w-full max-w-md" onSubmit={handleSubmit}>
                     <div className="flex gap-x-">
                         <label htmlFor="email-address" className="sr-only">
                             Email address
@@ -50,13 +52,13 @@ export default function Waitlist() {
                             name="email"
                             autoComplete="email"
                             value={email}
-                            className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-ginBlack shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-ginBlack    sm:text-sm sm:leading-6"
+                            className=" min-w-0 flex-auto rounded-md  border-2 px-3.5 py-2 placeholder:text-ginBlack text-ginBlack shadow-sm ring-1 ring-inset ring-pistachio focus:ring focus:ring-inset focus:ring-darkGreen focus:border-transparent"
                             placeholder="Enter your email here"
                             onChange={(e) => setEmail(e.target.value)}
                         />
                         <button
                             type="submit"
-                            className="flex-none rounded-md bg-pistachio px-3.5 py-2.5 text-sm font-semibold text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                            className="flex-none rounded-md bg-pistachio px-3.5 py-2.5 ml-8 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                         >
                             Notify me
                         </button>
