a76b72ef06dc02268b9f2bad3f636a088660f733
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-02 21:47:21 +0100

    feat(CommunityHero): Community component for Hero page setup

---
+++ components/CommunityHero/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CommunityHero = () => {
+  return (
+    <div>
+      <h2>Community</h2>
+      <p>
+        Welcome to Our Community Effort Against Invasive Green Iguanas! Are you
+        concerned about the threat posed by Green Iguanas as an Invasive Alien
+        Species (IAS)&#63; Whether you're a concerned citizen, a local farmer, a
+        conservationist, or a government official, we invite you to join our
+        collective effort to tackle this challenge!
+      </p>
+    </div>
+  );
+};
+
+export default CommunityHero;
