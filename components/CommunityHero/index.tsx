3c248b1cada23d8899891c8854ff65eab43418ca
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-02 22:27:34 +0100

    feat(learn more): button added to community hero component

--- components/CommunityHero/index.tsx
+++ components/CommunityHero/index.tsx
@@ -8,18 +8,25 @@ const CommunityHero = () => {
           Community
         </h1>
         <div className='grid grid-cols-2 gap-10 text-2xl'>
-        <p className=''>
-          Welcome to Our Community Effort Against Invasive Green Iguanas! Are
-          you concerned about the threat posed by Green Iguanas as an Invasive
-          Alien Species (IAS)&#63;
-        </p>
-        <p className=''>
-          Whether you're a concerned citizen, a local farmer, a conservationist,
-          or a government official, we invite you to join our collective effort
-          to tackle this challenge!
-        </p>
-
+          <p className=''>
+            Welcome to Our Community Effort Against Invasive Green Iguanas! Are
+            you concerned about the threat posed by Green Iguanas as an Invasive
+            Alien Species (IAS)&#63;
+          </p>
+          <p className=''>
+            Whether you're a concerned citizen, a local farmer, a
+            conservationist, or a government official, we invite you to join our
+            collective effort to tackle this challenge!
+          </p>
         </div>
+      <div className='mt-10 '>
+        <a
+          href='/invasive'
+          className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
+        >
+          Learn more
+        </a>
+      </div>
       </div>
       <div>
         <img
