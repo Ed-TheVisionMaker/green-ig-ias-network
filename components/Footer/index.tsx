37417bc2444f91bec138f0636f5d2f52b079e68d
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-04 22:27:25 +0100

    style: text

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,10 +3,14 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer>
-      <div>&copy; {new Date().getFullYear()} IAS-GIN Network</div>
-      <div>Contact: info@ias-gin.com</div>
+    <footer className='flex items-center justify-between font-open-sans w-full h-24 mt-8 border-t border-ginBlack'>
+      <b className='text-sm text-ginBlack pl-32'>
+        &copy; {new Date().getFullYear()} IAS-GIN Network
+      </b>
       <div>
+        <strong>Contact:</strong> info@ias-gin.com
+      </div>
+      <div className='text-sm font-bold text-ginBlack pr-32'>
         <Link href='/privacy-policy'>Privacy Policy</Link>
       </div>
     </footer>
