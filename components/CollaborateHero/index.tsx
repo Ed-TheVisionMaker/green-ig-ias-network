801125e3b7ac664cec65b8ab3bc106f51338453b
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-04 22:07:00 +0100

    feat(colloborate hero): added svgs and styled

--- components/CollaborateHero/index.tsx
+++ components/CollaborateHero/index.tsx
@@ -1,51 +1,69 @@
-import { ShareKnowledge } from '@/utils/svgs';
+import IdeaLightbulb, { Collaborate, ShareKnowledge } from '@/utils/svgs';
 import React from 'react';
 
 const CollaborateHero = () => {
-
   return (
-    <div>
-      <div>
-        <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
-          Collaborate - Share - Connect
-        </h1>
-        <div className='font-open-sans'>
-          <h2 className='font-bold text-3xl'>
-            Welcome to Our Collaborative Hub
-          </h2>
-          <p className='text-2xl mt-4'>
-            Join forces to pool knowledge and resources for effective IAS
-            management.
-          </p>
-        </div>
-        <div className='font-open-sans'>
-          <h2 className='font-bold text-3xl mt-8'>Find Ideas and Solutions</h2>
-          <p className='text-2xl mt-4'>
-            Explore innovative strategies and best practices to tackle IAS
-            populations.
-          </p>
-        </div>
+    <div className='mb-24'>
+      <div className='flex items-center'>
         <div>
-          <h2 className='font-bold text-3xl mt-8'>Support Each Other</h2>
-          <p className='text-2xl mt-4'>
-            Connect with a supportive community for advice, encouragement, and
-            practical assistance.
-          </p>
+          <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
+            Collaborate - Share - Connect
+          </h1>
+          <div className='font-open-sans'>
+            <h2 className='font-bold text-3xl'>
+              Welcome to Our Collaborative Hub
+            </h2>
+            <p className='text-2xl mt-4'>
+              Join forces to pool knowledge and resources for effective IAS
+              management.
+            </p>
+          </div>
+          <div className='font-open-sans'>
+            <h2 className='font-bold text-3xl mt-8'>
+              Find Ideas and Solutions
+            </h2>
+            <p className='text-2xl mt-4'>
+              Explore innovative strategies and best practices to tackle IAS
+              populations.
+            </p>
+          </div>
+          <div>
+            <h2 className='font-bold text-3xl mt-8'>Support Each Other</h2>
+            <p className='text-2xl mt-4'>
+              Connect with a supportive community for advice, encouragement, and
+              practical assistance.
+            </p>
+          </div>
+          <div>
+            <h2 className='font-bold text-3xl mt-8'>
+              Connect with Like-Minded Peers
+            </h2>
+            <p className='text-2xl mt-4'>
+              Forge meaningful connections with others passionate about IAS
+              management and biodiversity conservation.
+            </p>
+          </div>
         </div>
-        <div>
-          <h2 className='font-bold text-3xl mt-8'>
-            Connect with Like-Minded Peers
-          </h2>
-          <p className='text-2xl mt-4'>
-            Forge meaningful connections with others passionate about IAS
-            management and biodiversity conservation.
-          </p>
+        <div className='ml-8'>
+          <div>
+            {ShareKnowledge({
+              className:
+                'h-32 w-32 rounded-full border-2 border-ginBlack bg-teaGreen colour-ginBlack',
+            })}
+          </div>
+          <div>
+            {IdeaLightbulb({
+              className:
+                'h-32 w-32 rounded-full border-2 border-ginBlack mt-8 bg-teaGreen colour-ginBlack',
+            })}
+          </div>
+          <div>
+            {Collaborate({
+              className:
+                'h-32 w-32 rounded-full border-2 border-ginBlack mt-8 bg-teaGreen colour-ginBlack',
+            })}
+          </div>
         </div>
-      </div>
-      <div>
-        {ShareKnowledge({
-          className: 'h-32 w-32 rounded-full border-2 border-ginBlack',
-        })}
       </div>
     </div>
   );
