b6255ec4961ba8a1552f82e050b63746da3a12da
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-04 18:44:21 +0100

    feat(CollaborateHero): create component

---
+++ components/CollaborateHero/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const CollaborateHero = () => {
+  return (
+    <div>
+      <h1>Collaborate - Share - Connect</h1>
+      <div>
+        <h2>Welcome to Our Collaborative Hub</h2>
+        <p>
+          Are you passionate about addressing the challenges posed by Invasive
+          Alien Species (IAS) like the Green Iguana? Look no further&#33; Our
+          website is your dedicated platform for collaboration, resource
+          sharing, idea generation, mutual support, and connection within the
+          community of individuals and organizations committed to managing IAS.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CollaborateHero;
