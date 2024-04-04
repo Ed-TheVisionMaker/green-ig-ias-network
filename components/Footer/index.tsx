bb50fcdc3bbca804d7153cdedd503d55e16de9e7
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-04 23:08:18 +0100

    style: amended footer spacing and added box shadow

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,8 +5,8 @@ import WaitlistButton from '../WaitlistButton';
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col font-open-sans w-full h-24 mt-8'>
-      <div className='flex items-center mb-8'>
+    <footer className='flex flex-col font-open-sans w-full mt-24 shadow-xl shadow-ginBlack'>
+      <div className='flex items-center pt-8 bg-gradient-to-b from-teaGreen/50 to-ginWhite'>
         <div className='ml-32 rounded-xl w-max'>
           <img
             className='h-14 rounded-xl'
@@ -17,7 +17,7 @@ const Footer = () => {
         <WaitlistInput className=' min-w-0 rounded-md  border-2 px-3.5 py-2 ml-4 placeholder:text-ginBlack text-ginBlack shadow-sm ring-1 ring-inset ring-ginBlack focus:ring focus:ring-inset focus:ring-darkGreen focus:border-transparent' />
         <WaitlistButton className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 ml-8 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white' />
       </div>
-      <div className='h-24 mx-32 border-t border-ginBlack'></div>
+      <div className='h-2 mx-32 border-t border-ginBlack'></div>
       <div className='flex items-center justify-between py-8 bg-gradient-to-t from-teaGreen/50 to-ginWhite'>
         <div>
           <b className='text-sm text-ginBlack pl-32'>
