8a9bcbf20f43adda603622c90b07a8b14a671b15
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-04 22:59:20 +0100

    feat(waitlist button): created button

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import WailistInput from '../WaitlistInput';
+import WaitlistButton from '../WaitlistButton';
 
 const Footer = () => {
   return (
     <footer className='flex flex-col font-open-sans w-full h-24 mt-8'>
-      <div className='flex border border-pink items-center mb-8'>
-        <div className='ml-32 border border-teaGreen rounded-xl w-max'>
+      <div className='flex items-center mb-8'>
+        <div className='ml-32 rounded-xl w-max'>
           <img
             className='h-14 rounded-xl'
             src='/ias-gin-logo-white.png'
@@ -14,6 +15,7 @@ const Footer = () => {
           />
         </div>
         <WailistInput className=' min-w-0 rounded-md  border-2 px-3.5 py-2 ml-4 placeholder:text-ginBlack text-ginBlack shadow-sm ring-1 ring-inset ring-ginBlack focus:ring focus:ring-inset focus:ring-darkGreen focus:border-transparent' />
+        <WaitlistButton className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 ml-8 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white' />
       </div>
       <div className='h-24 mx-32 border-t border-ginBlack'></div>
       <div className='flex items-center justify-between py-8 bg-gradient-to-t from-teaGreen/50 to-ginWhite'>
---
+++ components/WaitlistButton/index.tsx
@@ -0,0 +1,15 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+const WaitlistButton = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      type='submit'
+    //   className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 ml-8 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
+    
+      {...props}>
+      Notify me
+    </button>
+  );
+};
+
+export default WaitlistButton;
