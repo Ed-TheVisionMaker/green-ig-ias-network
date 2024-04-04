69f6b29b292b5b40001d246080de4a20c66887e7
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-04 21:52:08 +0100

    feat(collaborate hero):added svg utils and rendered on UI

--- components/CollaborateHero/index.tsx
+++ components/CollaborateHero/index.tsx
@@ -1,40 +1,51 @@
+import { ShareKnowledge } from '@/utils/svgs';
 import React from 'react';
 
 const CollaborateHero = () => {
+
   return (
     <div>
-      <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
-        Collaborate - Share - Connect
-      </h1>
-      <div className='font-open-sans'>
-        <h2 className='font-bold text-3xl'>Welcome to Our Collaborative Hub</h2>
-        <p className='text-2xl mt-4'>
-          Join forces to pool knowledge and resources for effective IAS
-          management.
-        </p>
-      </div>
-      <div className='font-open-sans'>
-        <h2 className='font-bold text-3xl mt-8'>Find Ideas and Solutions</h2>
-        <p className='text-2xl mt-4'>
-          Explore innovative strategies and best practices to tackle IAS
-          populations.
-        </p>
+      <div>
+        <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
+          Collaborate - Share - Connect
+        </h1>
+        <div className='font-open-sans'>
+          <h2 className='font-bold text-3xl'>
+            Welcome to Our Collaborative Hub
+          </h2>
+          <p className='text-2xl mt-4'>
+            Join forces to pool knowledge and resources for effective IAS
+            management.
+          </p>
+        </div>
+        <div className='font-open-sans'>
+          <h2 className='font-bold text-3xl mt-8'>Find Ideas and Solutions</h2>
+          <p className='text-2xl mt-4'>
+            Explore innovative strategies and best practices to tackle IAS
+            populations.
+          </p>
+        </div>
+        <div>
+          <h2 className='font-bold text-3xl mt-8'>Support Each Other</h2>
+          <p className='text-2xl mt-4'>
+            Connect with a supportive community for advice, encouragement, and
+            practical assistance.
+          </p>
+        </div>
+        <div>
+          <h2 className='font-bold text-3xl mt-8'>
+            Connect with Like-Minded Peers
+          </h2>
+          <p className='text-2xl mt-4'>
+            Forge meaningful connections with others passionate about IAS
+            management and biodiversity conservation.
+          </p>
+        </div>
       </div>
       <div>
-        <h2 className='font-bold text-3xl mt-8'>Support Each Other</h2>
-        <p className='text-2xl mt-4'>
-          Connect with a supportive community for advice, encouragement, and
-          practical assistance.
-        </p>
-      </div>
-      <div>
-        <h2 className='font-bold text-3xl mt-8'>
-          Connect with Like-Minded Peers
-        </h2>
-        <p className='text-2xl mt-4'>
-          Forge meaningful connections with others passionate about IAS
-          management and biodiversity conservation.
-        </p>
+        {ShareKnowledge({
+          className: 'h-32 w-32 rounded-full border-2 border-ginBlack',
+        })}
       </div>
     </div>
   );
---
+++ utils/svgs.tsx
@@ -0,0 +1,89 @@
+import React, { SVGProps } from 'react';
+
+export function ShareKnowledge(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width='512'
+      height='512'
+      viewBox='0 0 512 512'
+      // style='color:currentColor'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <rect
+        width='512'
+        height='512'
+        x='0'
+        y='0'
+        rx='30'
+        fill='transparent'
+        stroke='transparent'
+        stroke-width='0'
+        stroke-opacity='100%'
+        paint-order='stroke'
+      ></rect>
+      <svg
+        width='256px'
+        height='256px'
+        viewBox='0 0 32 32'
+        fill='currentColor'
+        x='128'
+        y='128'
+        role='img'
+        // style='display:inline-block;vertical-align:middle'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <g fill='currentColor'>
+          <path
+            fill='currentColor'
+            d='M27 25h-6a3 3 0 0 0-3 3v2h2v-2a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2h2v-2a3 3 0 0 0-3-3zm-7-5a4 4 0 1 0 4-4a4 4 0 0 0-4 4zm6 0a2 2 0 1 1-2-2a2 2 0 0 1 2 2zM6 21v-1H4v1a7 7 0 0 0 7 7h3v-2h-3a5 5 0 0 1-5-5zm13-11h7v2h-7zm0-4h10v2H19zm0-4h10v2H19zm-8 9H5a3 3 0 0 0-3 3v2h2v-2a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2h2v-2a3 3 0 0 0-3-3zm-3-1a4 4 0 1 0-4-4a4 4 0 0 0 4 4zm0-6a2 2 0 1 1-2 2a2 2 0 0 1 2-2z'
+          />
+        </g>
+      </svg>
+    </svg>
+  );
+}
+
+export function Collaborate(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width='512'
+      height='512'
+      viewBox='0 0 512 512'
+      //   style='color:currentColor'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <rect
+        width='512'
+        height='512'
+        x='0'
+        y='0'
+        rx='30'
+        fill='transparent'
+        stroke='transparent'
+        stroke-width='0'
+        stroke-opacity='100%'
+        paint-order='stroke'
+      ></rect>
+      <svg
+        width='256px'
+        height='256px'
+        viewBox='0 0 32 32'
+        fill='currentColor'
+        x='128'
+        y='128'
+        role='img'
+        // style='display:inline-block;vertical-align:middle'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <g fill='currentColor'>
+          <path
+            fill='currentColor'
+            d='M6 21v-1H4v1a7 7 0 0 0 7 7h3v-2h-3a5 5 0 0 1-5-5zm18-10v1h2v-1a7 7 0 0 0-7-7h-3v2h3a5 5 0 0 1 5 5zm-13 0H5a3 3 0 0 0-3 3v2h2v-2a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2h2v-2a3 3 0 0 0-3-3zm-3-1a4 4 0 1 0-4-4a4 4 0 0 0 4 4zm0-6a2 2 0 1 1-2 2a2 2 0 0 1 2-2zm19 21h-6a3 3 0 0 0-3 3v2h2v-2a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2h2v-2a3 3 0 0 0-3-3zm-7-5a4 4 0 1 0 4-4a4 4 0 0 0-4 4zm6 0a2 2 0 1 1-2-2a2 2 0 0 1 2 2z'
+          />
+        </g>
+      </svg>
+    </svg>
+  );
+}
