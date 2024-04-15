96d5ec73bcd1309d6b7ac54031528186e1258962
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-15 17:36:51 +0100

    feat(hero page): wrapped sections in container component

--- components/CollaborateHero/index.tsx
+++ components/CollaborateHero/index.tsx
@@ -1,79 +1,82 @@
-import IdeaLightbulb, { Collaborate, ShareKnowledge } from '@/utils/svgs';
 import React from 'react';
+import Container from '@/components/Container';
+import IdeaLightbulb, { Collaborate, ShareKnowledge } from '@/utils/svgs';
 
 const CollaborateHero = () => {
   return (
-    <div className='mb-24'>
-      <div className='flex items-center'>
-        <div>
-          <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
-            Collaborate - Share - Connect
-          </h1>
-          <div className='font-open-sans'>
-            <h2 className='font-bold text-3xl'>
-              Welcome to Our Collaborative Hub
-            </h2>
-            <p className='text-2xl mt-4'>
-              Join forces to pool knowledge and resources for effective IAS
-              management.
-            </p>
-          </div>
-          <div className='font-open-sans'>
-            <h2 className='font-bold text-3xl mt-8'>
-              Find Ideas and Solutions
-            </h2>
-            <p className='text-2xl mt-4'>
-              Explore innovative strategies and best practices to tackle IAS
-              populations.
-            </p>
-          </div>
+    <Container>
+      <div className='mb-24'>
+        <div className='flex items-center'>
           <div>
-            <h2 className='font-bold text-3xl mt-8'>Support Each Other</h2>
-            <p className='text-2xl mt-4'>
-              Connect with a supportive community for advice, encouragement, and
-              practical assistance.
-            </p>
+            <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
+              Collaborate - Share - Connect
+            </h1>
+            <div className='font-open-sans'>
+              <h2 className='font-bold text-3xl'>
+                Welcome to Our Collaborative Hub
+              </h2>
+              <p className='text-2xl mt-4'>
+                Join forces to pool knowledge and resources for effective IAS
+                management.
+              </p>
+            </div>
+            <div className='font-open-sans'>
+              <h2 className='font-bold text-3xl mt-8'>
+                Find Ideas and Solutions
+              </h2>
+              <p className='text-2xl mt-4'>
+                Explore innovative strategies and best practices to tackle IAS
+                populations.
+              </p>
+            </div>
+            <div>
+              <h2 className='font-bold text-3xl mt-8'>Support Each Other</h2>
+              <p className='text-2xl mt-4'>
+                Connect with a supportive community for advice, encouragement,
+                and practical assistance.
+              </p>
+            </div>
+            <div>
+              <h2 className='font-bold text-3xl mt-8'>
+                Connect with Like-Minded Peers
+              </h2>
+              <p className='text-2xl mt-4'>
+                Forge meaningful connections with others passionate about IAS
+                management and biodiversity conservation.
+              </p>
+            </div>
+            <div className='mt-10 '>
+              <a
+                href='/collaborate'
+                className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
+              >
+                Learn more
+              </a>
+            </div>
           </div>
-          <div>
-            <h2 className='font-bold text-3xl mt-8'>
-              Connect with Like-Minded Peers
-            </h2>
-            <p className='text-2xl mt-4'>
-              Forge meaningful connections with others passionate about IAS
-              management and biodiversity conservation.
-            </p>
-          </div>
-          <div className='mt-10 '>
-            <a
-              href='/collaborate'
-              className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
-        <div className='ml-8'>
-          <div>
-            {ShareKnowledge({
-              className:
-                'h-32 w-32 rounded-full border-2 border-ginBlack bg-teaGreen colour-ginBlack',
-            })}
-          </div>
-          <div>
-            {IdeaLightbulb({
-              className:
-                'h-32 w-32 rounded-full border-2 border-ginBlack mt-8 bg-teaGreen colour-ginBlack',
-            })}
-          </div>
-          <div>
-            {Collaborate({
-              className:
-                'h-32 w-32 rounded-full border-2 border-ginBlack mt-8 bg-teaGreen colour-ginBlack',
-            })}
+          <div className='ml-8'>
+            <div>
+              {ShareKnowledge({
+                className:
+                  'h-32 w-32 rounded-full border-2 border-ginBlack bg-teaGreen colour-ginBlack',
+              })}
+            </div>
+            <div>
+              {IdeaLightbulb({
+                className:
+                  'h-32 w-32 rounded-full border-2 border-ginBlack mt-8 bg-teaGreen colour-ginBlack',
+              })}
+            </div>
+            <div>
+              {Collaborate({
+                className:
+                  'h-32 w-32 rounded-full border-2 border-ginBlack mt-8 bg-teaGreen colour-ginBlack',
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
--- components/InvasiveSpeciesHero/index.tsx
+++ components/InvasiveSpeciesHero/index.tsx
@@ -1,59 +1,63 @@
+import Container from '@/components/Container';
+
 export default function InvasiveSpeciesHero() {
   return (
-    <div className='bg-ginWhite rounded-xl my-24'>
-      <div className='flex'>
-        <div className='h-28 bg-gradient-to-r from-teaGreen/50 to-ginWhite rounded-lg'></div>
-        <div className=''>
-          <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
-            The Invasive Species
-          </h1>
-          <div className='flex relative'>
-            <div className='relative overflow-hidden'>
-              <div className=''>
-                <img
-                  className='rounded-xl shadow-2xl shadow-teaGreen ring-1 ring-gray-900/10'
-                  src='/green-ig-invasive.jpeg'
-                  alt=''
-                />
+    <Container>
+      <div className='bg-ginWhite rounded-xl my-24'>
+        <div className='flex'>
+          <div className='h-28 bg-gradient-to-r from-teaGreen/50 to-ginWhite rounded-lg'></div>
+          <div className=''>
+            <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
+              The Invasive Species
+            </h1>
+            <div className='flex relative'>
+              <div className='relative overflow-hidden'>
+                <div className=''>
+                  <img
+                    className='rounded-xl shadow-2xl shadow-teaGreen ring-1 ring-gray-900/10'
+                    src='/green-ig-invasive.jpeg'
+                    alt=''
+                  />
+                </div>
               </div>
-            </div>
               <div className='flex flex-col W-1/2 pl-8 text-xl'>
-              <div>
-                <p>
-                  The Green Iguana (Iguana iguana), known for its vibrant green
-                  coloration and distinctive dewlap, is a reptile native to
-                  Central and South America. It is a species of lizard in the
-                  family Iguanidae, characterized by its long tail, strong
-                  limbs, and sharp claws.
-                </p>
-                <p className='mt-8'>
-                  Historically, it has been a popular pet due to its docile
-                  nature, but its introduction into non-native habitats has led
-                  to significant ecological and economic challenges.
-                </p>
+                <div>
+                  <p>
+                    The Green Iguana (Iguana iguana), known for its vibrant
+                    green coloration and distinctive dewlap, is a reptile native
+                    to Central and South America. It is a species of lizard in
+                    the family Iguanidae, characterized by its long tail, strong
+                    limbs, and sharp claws.
+                  </p>
+                  <p className='mt-8'>
+                    Historically, it has been a popular pet due to its docile
+                    nature, but its introduction into non-native habitats has
+                    led to significant ecological and economic challenges.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Several factors contribute to the Green Iguana's status as
+                    an Invasive Alien Species (IAS):
+                  </p>
+                  <p className='mt-8'>
+                    Introduction as Pets, Escape and Release and Habitat
+                    Adaptability
+                  </p>
+                </div>
+                <div className='absolute bottom-2 mt-10 '>
+                  <a
+                    href='/invasive'
+                    className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
+                  >
+                    Learn more
+                  </a>
+                </div>
               </div>
-              <div>
-                <p>
-                  Several factors contribute to the Green Iguana's status as an
-                  Invasive Alien Species (IAS):
-                </p>
-                <p className='mt-8'>
-                  Introduction as Pets, Escape and Release and Habitat
-                  Adaptability
-                </p>
-              </div>
-            <div className='absolute bottom-2 mt-10 '>
-              <a
-                href='/invasive'
-                className="flex-none rounded-md bg-pistachio px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              >
-                Learn more
-              </a>
-            </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
