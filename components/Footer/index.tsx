be144fb36560bc8beb2e82edd4e4749067948bc5
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-15 21:58:46 +0100

    style: increased logo size in footer and altered text size/layout

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,7 +7,10 @@ const Footer = () => {
   return (
     <footer className='flex flex-col font-open-sans w-full mt-24 shadow-xl shadow-ginBlack'>
       <div className='flex items-center justify-between py-8 bg-gradient-to-r from-teaGreen/50 to-ginWhite'>
-        <div className='flex flex-col ml-32 px-12 py-8 border-2 border-ginWhite rounded-xl'>
+        {/* <div className='flex flex-col ml-40 px-12 py-8 border-2 border-ginWhite rounded-xl'> */}
+        <div
+        
+        className='flex flex-col ml-40'>
           <h3
           className='text-3xl font-ubuntu font-bold tracking-tight text-ginBlack mb-4'>Join our waitlist</h3>
           <div className='flex'>
@@ -15,18 +18,17 @@ const Footer = () => {
             <WaitlistButton className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 ml-8 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white' />
           </div>
         </div>
-        <div className='flex items-center rounded-xl w-max'>
+        <div className='flex flex-col items-center rounded-xl w-max'>
           <img
-            className='h-14 rounded-xl border border-teaGreen'
+            className='h-28 rounded-xl border border-teaGreen'
             src='/ias-gin-logo-white.png'
             alt='IAS-GIN Network'
           />
-          <div className='flex flex-col ml-4 leading-snug text-ginBlack text-xs font-semibold'>
-            <p className=''>Connect</p>
-            <p className=''>Share</p>
-            <p className=''>Collaborate</p>
+          <div className='ml-4 leading-loose text-ginBlack text-md font-semibold'>
+            <p>Connect &minus; Share &minus; Collaborate</p>
           </div>
-          <div className='flex ml-48 text-sm text-ginBlack font-semibold'>
+        </div>
+          <div className='flex mr-40 text-sm text-ginBlack font-semibold'>
             <div className=' flex flex-col'>
               <Link
                 className='w-fit border-b border-transparent hover:border-ginBlack mb-2'
@@ -56,19 +58,18 @@ const Footer = () => {
               </Link>
             </div>
           </div>
-        </div>
       </div>
       <div className='border-t border-dotted border-ginBlack'></div>
       <div className='flex items-center justify-between py-4 bg-gradient-to-r from-teaGreen/50 to-ginWhite'>
         <div>
-          <b className='text-sm text-ginBlack pl-32'>
+          <b className='text-sm text-ginBlack pl-40'>
             &copy; {new Date().getFullYear()} IAS-GIN Network
           </b>
         </div>
         <div>
           <strong>Contact:</strong> info@ias-gin.com
         </div>
-        <div className='text-sm font-bold text-ginBlack pr-32'>
+        <div className='text-sm font-bold text-ginBlack pr-40'>
           <Link href='/privacy-policy'>Privacy Policy</Link>
         </div>
       </div>
