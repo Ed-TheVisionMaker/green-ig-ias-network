0039aec20c5f33bb2dc8eae2c1384c1f56b4477a
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-24 17:03:47 +0100

    style: new invasive species hero text styled

--- components/InvasiveSpeciesHero/index.tsx
+++ components/InvasiveSpeciesHero/index.tsx
@@ -10,8 +10,8 @@ export default function InvasiveSpeciesHero() {
             <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
               The Invasive Species
             </h1>
-            <div className='flex relative'>
-              <div className='relative'>
+            <div className='flex items-center'>
+              <div className='w-fit'>
                 <div className=''>
                   <img
                     className='rounded-xl shadow-xl shadow-ginBlack/50 ring-1 ring-gray-900/10'
@@ -20,20 +20,19 @@ export default function InvasiveSpeciesHero() {
                   />
                 </div>
               </div>
-              <div className='flex flex-col W-1/2 pl-8 text-xl'>
+              <div className='relative flex flex-col pl-8 text-xl'>
                 <div>
-                  <p className='mt-8'>
+                  <p className=''>
                     The Green Iguana (Iguana iguana) is a vibrant reptile native
                     to Central and South America, known for its striking green
                     color and unique dewlap.
                   </p>
                   <p className='mt-8'>
                     Introduction to non-native habitats has caused major
-                    ecological and economic issues. It's classified as an
-                    Invasive
+                    ecological and economic issues.
                   </p>
-                  <p>
-                    Alien Species (IAS) due to:
+                  <p className='mt-8'>
+                    It's classified as an Invasive Alien Species (IAS) due to:
                     <ul className='ml-8'>
                       <li className='mt-8'>Being introduced as pets</li>
                       <li className='mt-8'>Escapes and releases </li>
@@ -43,7 +42,7 @@ export default function InvasiveSpeciesHero() {
                     </ul>
                   </p>
                 </div>
-                <div className='absolute bottom-2 mt-10 '>
+                <div className='absolute right-0 bottom-2 mt-10 '>
                   <a
                     href='/invasive'
                     className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
