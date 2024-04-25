4f4c81b682f7a5d1c42cf3a29edfd57b4fd6e814
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-04-25 11:33:32 +0100

    style: amended styling on landing page

--- components/CollaborateHero/index.tsx
+++ components/CollaborateHero/index.tsx
@@ -5,13 +5,13 @@ import { IdeaLightbulb, Collaborate, ShareKnowledge } from '@/utils/svgs';
 const CollaborateHero = () => {
   return (
     <Container>
-      <div className='relative mb-24'>
+      <div className='mb-24'>
         <div className='flex items-center'>
           <div>
             <h1 className='mt-2 mb-8 text-5xl font-ubuntu font-bold tracking-tight text-ginBlack'>
               Collaborate - Share - Connect
             </h1>
-            <div className='border-4 border-dotted border-ginBlack px-8 pb-32 pt-12 rounded-xl'>
+            <div className='border border-ginBlack px-16 py-12 rounded-xl'>
               <div className='font-open-sans text-center mb-8'>
                 <h2 className='font-bold text-4xl'>
                   Welcome to Our Collaborative Hub
@@ -48,36 +48,36 @@ const CollaborateHero = () => {
                       IAS management and biodiversity conservation.
                     </p>
                   </div>
-                  <div className='absolute bottom-12 left-1/2'>
-                    <a
-                      href='/collaborate'
-                      className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
-                    >
-                      Learn more
-                    </a>
-                  </div>
                 </div>
                 <div className='mt-8 ml-8'>
                   <div>
                     {IdeaLightbulb({
                       className:
-                        'h-32 w-32 rounded-full border-2 border-ginBlack bg-teaGreen colour-ginBlack shadow-md shadow-ginBlack/50',
+                        'h-32 w-32 rounded-full  bg-teaGreen colour-ginBlack shadow-md shadow-ginBlack/50',
                     })}
                   </div>
                   <div>
                     {ShareKnowledge({
                       className:
-                        'h-32 w-32 rounded-full border-2 border-ginBlack mt-8 bg-teaGreen colour-ginBlack shadow-md shadow-ginBlack/50',
+                        'h-32 w-32 rounded-full mt-8 bg-teaGreen colour-ginBlack shadow-md shadow-ginBlack/50',
                     })}
                   </div>
                   <div>
                     {Collaborate({
                       className:
-                        'h-32 w-32 rounded-full border-2 border-ginBlack mt-8 bg-teaGreen colour-ginBlack shadow-md shadow-ginBlack/50',
+                        'h-32 w-32 rounded-full  mt-8 bg-teaGreen colour-ginBlack shadow-md shadow-ginBlack/50',
                     })}
                   </div>
                 </div>
               </div>
+            <div className='flex justify-center w-full mt-24'>
+              <a
+                href='/collaborate'
+                className='rounded-md bg-pistachio mx-auto px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
+              >
+                Learn more
+              </a>
+            </div>
             </div>
           </div>
         </div>
