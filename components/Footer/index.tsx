7ab9fd19288b92cf05869e07ce4cd94947da283b
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-05-02 23:07:03 +0100

    style: rearrange footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,32 +6,21 @@ import WaitlistButton from '../WaitlistButton';
 const Footer = () => {
   return (
     <footer className='flex flex-col font-open-sans w-full mt-24 shadow-xl shadow-ginBlack'>
-      <div className='flex items-center justify-between py-8 bg-gradient-to-r from-teaGreen/50 to-ginWhite'>
-        <div
-        
-        className='flex flex-col ml-40'>
-          <h3
-          className='text-3xl font-ubuntu font-bold tracking-tight text-ginBlack mb-4'>Join our waitlist</h3>
-          <div className='flex'>
-            <WaitlistInput className=' min-w-0 rounded-md  border-2 px-3.5 py-2 placeholder:text-ginBlack text-ginBlack shadow-sm ring-1 ring-inset ring-ginBlack focus:ring focus:ring-inset focus:ring-darkGreen focus:border-transparent' />
-            <WaitlistButton className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 ml-8 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white' />
-          </div>
-        </div>
-        <div className='flex flex-col items-center mr-40 rounded-xl w-max'>
-          <img
-            className='h-28 rounded-xl border mt-1 mb-2 border-teaGreen'
-            src='/ias-gin-logo-white.png'
-            alt='IAS-GIN Network'
-          />
-          <div className='ml-4 leading-loose text-ginBlack text-md font-semibold'>
-            <p>Connect &minus; Share &minus; Collaborate</p>
-          </div>
-        </div>
-          <div className='flex flex-col mr-40 text-md text-ginBlack font-semibold'>
-            <h3
-            className='text-xl mb-2 font-ubuntu font-bold leading-1 text-ginBlack'>
-              Navigate
-            </h3>
+      <div className='flex'>
+        <div className='flex items-center justify-between py-8 bg-gradient-to-r from-teaGreen/50 to-ginWhite'>
+          <div className='flex items-center mr-40 rounded-xl w-max'>
+            <img
+              className='h-28 mt-1 mb-2'
+              src='/ias-gin-logo-black.png'
+              alt='IAS-GIN Network'
+            />
+            <div className='ml-4 leading-loose text-ginBlack text-md font-semibold'>
+              <p>Connect &minus; Share &minus; Collaborate</p>
+            </div>
+            <div className='flex flex-col mr-40 text-md text-ginBlack font-semibold'>
+              <h3 className='text-xl mb-2 font-ubuntu font-bold leading-1 text-ginBlack'>
+                Navigate
+              </h3>
               <Link
                 className='w-fit border-b border-transparent font-medium leading-1 hover:border-ginBlack'
                 href='/community'
@@ -62,20 +51,32 @@ const Footer = () => {
               >
                 Latest Information
               </Link>
+            </div>
           </div>
-      </div>
-      <div className='border-t border-dotted border-ginBlack'></div>
-      <div className='flex items-center justify-between py-4 bg-gradient-to-r from-teaGreen/50 to-ginWhite'>
-        <div>
-          <b className='text-sm text-ginBlack pl-40'>
-            &copy; {new Date().getFullYear()} IAS-GIN Network
-          </b>
+          <div className='border-t border-dotted border-ginBlack'></div>
+          <div className='flex items-center justify-between py-4 bg-gradient-to-r from-teaGreen/50 to-ginWhite'></div>
+          <div className='flex flex-col ml-40'>
+            <h3 className='text-3xl font-ubuntu font-bold tracking-tight text-ginBlack mb-4'>
+              Join our waitlist
+            </h3>
+            <div className='flex'>
+              <WaitlistInput className=' min-w-0 rounded-md  border-2 px-3.5 py-2 placeholder:text-ginBlack text-ginBlack shadow-sm ring-1 ring-inset ring-ginBlack focus:ring focus:ring-inset focus:ring-darkGreen focus:border-transparent' />
+              <WaitlistButton className='flex-none rounded-md bg-pistachio px-3.5 py-2.5 ml-8 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white' />
+            </div>
+          </div>
         </div>
+      </div>
+      <div className='flex'>
         <div>
           <strong>Contact:</strong> info@ias-gin.com
         </div>
         <div className='text-sm font-bold text-ginBlack pr-40 mr-14'>
           <Link href='/privacy-policy'>Privacy Policy</Link>
+        </div>
+        <div>
+          <b className='text-sm text-ginBlack pl-40'>
+            &copy; {new Date().getFullYear()} IAS-GIN Network
+          </b>
         </div>
       </div>
     </footer>
