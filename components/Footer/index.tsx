d5fecc1c729a97cbdc33f85cf3fe62bd0c36f7ef
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-05-07 08:58:17 +0100

    style: remove text and improve alignment

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,7 +5,7 @@ import WaitlistButton from '../WaitlistButton';
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col font-open-sans w-full mt-24 px-24 bg-ginWhite border-t border-ginBlack/10'>
+    <footer className='flex flex-col font-open-sans w-full mt-24 px-24 mb-4 bg-ginWhite border-t border-ginBlack/10'>
       <div className='flex w-full items-center justify-between py-8'>
         <div className='flex items-center'>
           <img
@@ -13,11 +13,6 @@ const Footer = () => {
             src='/ias-gin-logo-black.png'
             alt='IAS-GIN Network'
           />
-          {/* <div className='flex flex-col text-ginBlack text-md font-semibold leading-loose'>
-            <p className=''>Connect</p>
-            <p>Share</p>
-            <p>Collaborate</p>
-          </div> */}
         </div>
         <div className='flex flex-col ml-40 text-md text-ginBlack font-semibold'>
           <h3 className='text-md mb-2 font-ubuntu font-bold text-ginBlack'>
@@ -66,19 +61,16 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className='flex flex-col items-end'>
-          {/* <h3 className='text-sm font-ubuntu tracking-tight text-ginBlack mb-2'>
-            Register here
-          </h3> */}
-          <WaitlistInput className=' min-w-0 rounded-md  h-8 border-2 px-3.5 py-2 mb-2 placeholder:text-ginBlack text-ginBlack shadow-sm ring-1 ring-inset ring-ginBlack focus:ring focus:ring-inset focus:ring-darkGreen focus:border-transparent' />
-          <WaitlistButton className='rounded-md bg-aquamarine px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white' />
+        <div className='h-full flex flex-col justify-between items-end'>
+          <WaitlistInput className=' min-w-0 rounded-md h-8 border-2 px-3.5 py-2 mb-4 placeholder:text-ginBlack text-ginBlack shadow-sm ring-1 ring-inset ring-ginBlack focus:ring focus:ring-inset focus:ring-darkGreen focus:border-transparent' />
+          <WaitlistButton className='rounded-md bg-aquamarine px-3.5 py-2.5 text-sm font-semibold text-ginBlack placeholder:text-ginBlack shadow-sm focus-visible:outline focus-visible:outline-offset focus-visible:outline-white' />
         </div>
       </div>
       <div className='flex justify-between'>
         <div className='ml-6'>
           <strong>Contact:</strong> info@ias-gin.com
         </div>
-        <div className='text-sm font-bold text-ginBlack mr-90'>
+        <div className='text-sm font-bold text-ginBlack'>
           <Link href='/privacy-policy'>Privacy Policy</Link>
         </div>
         <div>
